a2d0ce7462720cb080dc8a7ba4bbc04f09ae0812
Anmol Ahuja <me@anmolahuja.com>
2024-03-04 19:03:39 -0800

    move process.py inputs to config files

--- process/papyrus_pipeline/config.py
+++ process/papyrus_pipeline/config.py
@@ -1,7 +1,7 @@
 import yaml
 
 
-class Config:
+class PapyrusConfig:
     def __init__(self, file_path: str) -> None:
         with open("config.yaml") as f:
             config = yaml.safe_load(f)
@@ -11,4 +11,6 @@ class Config:
         self.connection_args = {"host": self.milvus_endpoint, "port": f'{self.port}'}
         self.milvus_address=f'{self.milvus_endpoint}:{self.port}'
 
-        self.openai_key = config['openai']['key']
+        self.video_file_dir = config['video_file_dir']
+        self.output_dir = config['output_dir']
+        self.processed_video_dir_path = config['processed_video_dir_path']
